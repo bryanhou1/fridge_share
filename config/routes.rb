e8b9546396cc0e20ca6aa3f4737c69596db1aa9b
Bryan Hou <bryan.hou94@gmail.com>
2017-09-27 10:24:08 -0400

    add nested items routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   resources :items
   resources :fridges
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
+  resource :user do
+	  resources :items
+	end
+
   root 'welcome#index'
   # get '/auth/facebook/callback' => 'sessions#create'
   get '/about' => 'welcome#about'
   get '/users' => 'users#index'
-  get '/items' => 'item#index'
-  
+
+
 end
