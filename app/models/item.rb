93fffde5ca1eac1497c89c5be3b9505112f02337
Bryan Hou <bryan.hou94@gmail.com>
2017-09-30 14:36:34 -0400

    fix Item.expired_items logic

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,21 +6,22 @@ class Item < ApplicationRecord
 
 	def self.expired_items
 		@today = today
-		all.collect do |item| 
+
+		return all.collect { |item| 
 			expiration_date_arr = item.expiration_date.scan(/.{1,2}/).map { |s| s.to_i }
 
 			if (expiration_date_arr[2] < @today[2]) || (expiration_date_arr[2] == @today[2] && expiration_date_arr[0] < @today[0]) || (expiration_date_arr[2] == @today[2] && expiration_date_arr[0] == @today[0] && expiration_date_arr[1] < @today[1])
 				item
 			end
-		end
+		}.uniq
+		
 	end
-
 
 	private
 
 	def self.today
 		today_date = Date.today
-		[today_date.year, today_date.mon, today_date.day]
+		[today_date.mon, today_date.day, today_date.year-2000]
 	end
 
 end
