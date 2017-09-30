d99bc86bc619697d14824aa48c38e7695d6f0d45
Bryan Hou <bryan.hou94@gmail.com>
2017-09-30 14:17:34 -0400

    add Item class method expired_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,25 @@ class Item < ApplicationRecord
 	belongs_to :user
 	belongs_to :fridge
 	validates_with ExpirationDateValidator, attributes: :expiration_date
+
+
+	def self.expired_items
+		@today = today
+		all.collect do |item| 
+			expiration_date_arr = item.expiration_date.scan(/.{1,2}/).map { |s| s.to_i }
+
+			if (expiration_date_arr[2] < @today[2]) || (expiration_date_arr[2] == @today[2] && expiration_date_arr[0] < @today[0]) || (expiration_date_arr[2] == @today[2] && expiration_date_arr[0] == @today[0] && expiration_date_arr[1] < @today[1])
+				item
+			end
+		end
+	end
+
+
+	private
+
+	def self.today
+		today_date = Date.today
+		[today_date.year, today_date.mon, today_date.day]
+	end
+
 end
