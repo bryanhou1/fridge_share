441952271d88b647206945ff8f75f892049c0da3
Bryan Hou <bryan.hou94@gmail.com>
2017-12-04 23:07:43 -0500

    build showItems() in items.js

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -6,14 +6,36 @@ $(() => {
 });
 
 function getItems() {
-
+	return $.get("/items.json", items => {
+		store.state.items = items.map(attributes => new Item(attributes))
+	})
 }
 
 function showItems() {
+	const element = $("ul#items_li");
+	const itemElements = store.getState().items.reduce((pre, next) =>  pre + next.toHtmlLi(), "");
+
+	element.empty();
+	element.append(itemElements)
+
+	showItemListener();
+	editItemListener();
+	destroyItemListener();
+}
+
+function addNewItemBtnListener() {
 
 }
 
-addNewItemBtnListener() {
+function showItemListener() {
+
+}
+
+function editItemListener() {
+
+}
+
+function destroyItemListener() {
 
 }
 
@@ -23,5 +45,17 @@ class Item {
 		this.name = attr.name;
 		this.expiration_date = attr.expiration_date;
 		this.user = attr.user;
+	}
+
+	toHtmlLi(){
+		return `
+			<li>
+				<h4> ${this.name} </h4> 
+				Expiration Date: ${this.expiration_date} <br>
+				Belongs_to: <a href="/users/${this.user.id}">${this.user.name}</a> <br>
+				<a class="show_item_btn" data-item-id="${this.id}">Show</a> | 
+				<a class="edit_item_btn" data-item-id="${this.id}">Edit</a> | 
+				<a class="destroy_item_btn" data-item-id="${this.id}">Destroy</a>
+			</li>`;
 	}
 }
