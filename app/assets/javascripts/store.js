dcccd97d2f4385591ecb7813fbb2026ef585cc3a
Bryan Hou <bryan.hou94@gmail.com>
2017-12-08 16:06:52 -0500

    build dispatch and reducer function

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -9,14 +9,23 @@ function configureStore () {
 		return state;
 	}
 
-	function dispatch(actionCreator) {
-		var action = actionCreator()
-		reducer(action)
-	},
+	function dispatch(action) {
+		state = reducer(state, action);
+	}
 	
-	function reducer(ob) {
+	function reducer(state, action) {
+		switch (action.type) {
+			case "ADD_FRIDGES":
+				return {...state, action.payload};
+			case "ADD_ITEMS":
+				return {...state, action.payload};
+			case "ADD_USERS":
+				return {...state, action.payload};
+			default:
+				return state;
+		}
 
-	},
+	}
 
 	return { 
 		getState,
