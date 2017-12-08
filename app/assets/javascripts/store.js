2dabde3b45363c876e2470ef5169394825067f44
Bryan Hou <bryan.hou94@gmail.com>
2017-12-08 13:54:26 -0500

    rebuilding store using IIFE closure using redux design pattern

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,17 +1,28 @@
-const store = {
-	state: {
+function configureStore () {
+	let state = {
 		fridges: [],
 		items: [],
 		users: [],
-	},
-	getState: function() {
-		return this.state
-	},
-	// dispatch: function(actionCreator) {
-	// 	var action = actionCreator()
-	// 	this.reducer(action)
-	// },
-	// reducer: function(ob) {
+	}
 
-	// }
+	function getState() {
+		return state;
+	}
+
+	function dispatch(actionCreator) {
+		var action = actionCreator()
+		reducer(action)
+	},
+	
+	function reducer(ob) {
+
+	},
+
+	return { 
+		getState,
+		dispatch,
+	}
 }
+
+const store = configureStore();
+
