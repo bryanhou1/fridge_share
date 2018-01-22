50c77b19816c333941aca0036029199b116fa40d
Bryan Hou <bryan.hou94@gmail.com>
2018-01-22 18:33:58 -0500

    switch sqlite3 for pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171002033451) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "fridges", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -23,8 +26,8 @@ ActiveRecord::Schema.define(version: 20171002033451) do
     t.string "expiration_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "fridge_id"
+    t.bigint "user_id"
+    t.bigint "fridge_id"
     t.index ["fridge_id"], name: "index_items_on_fridge_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
@@ -50,4 +53,6 @@ ActiveRecord::Schema.define(version: 20171002033451) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "fridges"
+  add_foreign_key "items", "users"
 end
